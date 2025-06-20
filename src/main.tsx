57cd59687647d84b7ef063e14ce4fb817cd0b552
signature13s <mukulks1409@gmail.com>
2025-06-20 19:26:00 +0530

    errors fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 // src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { MantineProvider } from '@mantine/core';
-import { Notifications } from '@mantine/notifications';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
